65c47f98a667870df57a35996426bd8b7fde4396
Equinox <Equinox@users.noreply.github.com>
2020-10-31 15:49:20 +1300

    fix typo

--- prettierPlease.js
+++ prettierPlease.js
@@ -83,7 +83,7 @@ const prettierPlease = async () => {
 
   fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
 
-  console.log(`Sucess! Added Prettier to project ${packageJson.name}.`);
+  console.log(`Success! Added Prettier to project ${packageJson.name}.`);
   console.log();
   console.log("I suggest that you format your existing code by typing:");
   console.log();
